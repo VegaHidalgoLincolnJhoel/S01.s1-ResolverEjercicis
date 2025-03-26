b3aa82f6ba5d4ec315f0cd40f5e6236dcd77aa5e
LAB-USR-LNORTE <LAB-USR-LNORTE@UTP362640.grupoutp.net>
2025-03-26 13:32:01 -0500

    Practica

---
+++ JavaApplication8/src/Semana_1/Celular.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Semana_1;
+
+/**
+ *
+ * @author LAB-USR-LNORTE
+ */
+public class Celular {
+    
+    public static void main(String[] args) {
+        String marca;
+        double costo;
+        int anioFabricacion;
+        boolean importado;
+        char marcador;
+    }
+    
+}
